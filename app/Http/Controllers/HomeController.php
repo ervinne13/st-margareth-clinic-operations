da26870768ecd40f7e4deff4496bcd92ebb7532b
ervinne13 <ervinne.sodusta@gs3.com.ph>
2017-08-28 21:47:35 +0800

    WIP

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use function view;
+
+class HomeController extends Controller {
+
+    public function index() {
+        return view('skarla-tester');
+    }
+
+}
