f78f25e095ea2d558f25c1aa1dfd55a919856475
ervinne13 <ervinne.sodusta@gs3.com.ph>
2017-09-09 16:31:26 +0800

    Add functional design files

---
+++ functional-design-files/run st-margareth-clinic-operations.bat
@@ -0,0 +1,40 @@
+
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------   
+
+set todaysdate=%date:~-10%
+
+date 03/15/2017
+REM --> start "" "C:\Program Files (x86)\DbSchema\DbSchema.exe"
+start "" "C:\Program Files (x86)\DbSchema\DbSchema.exe" "C:\xampp\htdocs\st-margareth-clinic-operations\functional-design-files\st-margareth-clinic-operations.dbs"
+
+timeout 30
+date %todaysdate%
