8bfdf154f0327119ee44fbc3712ca4984f8d804e
ervinne <ervinne.sodusta@gs3.com.ph>
2017-09-27 20:03:34 +0800

    WIP: Squash later

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,18 +4,18 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
-{
+class CreateUsersTable extends Migration {
+
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
+    public function up() {
+        Schema::create('user_account', function (Blueprint $table) {
+            $table->string('username', 30)->primary();
+            $table->boolean('is_active');
+            $table->boolean('has_location_full_access');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
@@ -28,8 +28,8 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('users');
     }
+
 }
